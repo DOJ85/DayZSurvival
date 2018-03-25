5e13eeb0b8415566f26ca31481dfb55b1a2f0dfc
Unknown <florianpelloux85@gmail.com>
2018-03-25 19:57:17 +0200

    0.0.21
    test2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function randombg(){
   var random= Math.floor(Math.random() * 18) + 0;
-  var bigSize = ["url('img/img1.jpg')",
+  var bigSize = ["url('img/img10.jpg')",
                  "url('img/img2.jpg')",
                  "url('img/img3.jpg')",
                  "url('img/img4.jpg')",
@@ -9,7 +9,7 @@ function randombg(){
                  "url('img/img7.jpg')",
                  "url('img/img8.jpg')",
                  "url('img/img9.jpg')",
-                 "url('img/img10.jpg')",
+                 "url('img/img1.jpg')",
                  "url('img/img11.jpg')",
                  "url('img/img12.jpg')",
                  "url('img/img13.jpg')",
